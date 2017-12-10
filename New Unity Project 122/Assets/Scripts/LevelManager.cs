de457d8eaa7e739dd8ad4145fec4c2a79a48038c
Zpiralh <elias.wirtanen@gmail.com>
2017-12-10 06:41:01 +0200

    First commit
    Added Tilemap

---
+++ New Unity Project 122/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    // Prefab yhdelle tilelle
+    [SerializeField]
+    private GameObject[] tilePrefabs;
+
+    // Palauttaa tilen koon 
+    public float TileSize
+    {
+        get { return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        CreateLevel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    // Tekee levelin
+    private void CreateLevel()
+    {
+
+        string[] mapData = ReadLevelText();
+
+        //laskee kentän x koon
+        int mapX = mapData[0].ToCharArray().Length;
+        //laskee kentän y koon
+        int mapY = mapData.Length;
+
+        Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+
+        for (int y = 0; y < mapY; y++) // Y:n sijainti
+        {
+
+            char[] newTiles = mapData[y].ToCharArray();
+
+            for (int x = 0; x < mapX; x++) // X:n sijainti
+            {
+                PlaceTile(newTiles[x].ToString(), x, y, worldStart);
+            }
+        }
+    }
+
+    private void PlaceTile(string tileType, int x, int y, Vector3 worldStart)
+    {
+        int tileIndex = int.Parse(tileType);
+        // Tekee uuden tilen ja viittaa siihen newTile muuttujassa
+        GameObject newTile = Instantiate(tilePrefabs[tileIndex]);
+
+        // Käyttää newTile muuttujaa muuttaakseen tilen sijaintia
+        newTile.transform.position = new Vector3(worldStart.x + (TileSize * x), worldStart.y - (TileSize * y), 0);
+    }
+
+    private string[] ReadLevelText()
+    {
+        TextAsset bindData = Resources.Load("Levell.txt") as TextAsset;
+
+        string data = bindData.text.Replace(Environment.NewLine, string.Empty);
+
+        return data.Split('-');
+    }
+}
